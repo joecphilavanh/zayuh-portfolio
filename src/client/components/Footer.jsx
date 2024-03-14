ee5756faeb6fcc5248d993d92fd4b5d72fe43a30
Joe Philavanh <joecphilavanh@yahoo.com>
2024-03-14 13:03:19 -0700

    fixed footer

--- src/client/components/Footer.jsx
+++ src/client/components/Footer.jsx
@@ -3,43 +3,35 @@ import "../style/Footer.css";
 
 const Footer = () => (
   <footer className="site-footer">
-    <div className="footer-content">
-      <div className="text-container">
-        <p>
-          "Creativity is seeing what everyone else has seen, and thinking what
-          no one else has thought." - Albert Einstein
-        </p>
-      </div>
-      <div className="social-links">
-        <a
-          href="https://www.youtube.com/@hellakawaii193"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="youtube-link"
-        >
-          <img src="/img/youtube.png" alt="YouTube" className="youtube-icon" />
-        </a>
-        <a
-          href="https://www.instagram.com/zayuhh/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="instagram-link"
-        >
-          <img
-            src="/img/instagram.png"
-            alt="Instagram"
-            className="instagram-icon"
-          />
-        </a>
-        <a
-          href="https://twitter.com/Zayuhh"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="x-link"
-        >
-          <img src="/img/x.png" alt="x" className="x-icon" />
-        </a>
-      </div>
+    <div className="social-links">
+      <a
+        href="https://www.youtube.com/@hellakawaii193"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="youtube-link"
+      >
+        <img src="/img/youtube.png" alt="YouTube" className="youtube-icon" />
+      </a>
+      <a
+        href="https://www.instagram.com/zayuhh/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="instagram-link"
+      >
+        <img
+          src="/img/instagram.png"
+          alt="Instagram"
+          className="instagram-icon"
+        />
+      </a>
+      <a
+        href="https://twitter.com/Zayuhh"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="x-link"
+      >
+        <img src="/img/x.png" alt="x" className="x-icon" />
+      </a>
     </div>
   </footer>
 );
